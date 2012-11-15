aa3a5d6c2fb532a7cb5062a2b6bb7e2487ce8c79
Boram Yoon <boram@me.com>
2012-11-14 19:12:02 -0800

    Add regex criteria option

--- spec/trejo/view_helpers_spec.rb
+++ spec/trejo/view_helpers_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+
 include ActionView::Helpers::UrlHelper
 class TrejoHelperTestClass; end
 
@@ -36,12 +37,31 @@ describe Trejo::ViewHelpers do
       end
     end
 
-    context 'with a selected regex' do
+    context 'with a selection criteria regex' do
       before do
         request = stub fullpath: '/home?page=2'
         @trejo.stub(:request).and_return(request)
       end
 
+      context 'when the current page matches the url' do
+        before do
+          @nav_item = @trejo.nav_item('Home', '/home', selected: /^\/home/)
+        end
+
+        it 'returns a selected nav item' do
+          expect(@nav_item).to eq('<a href="/home" class="active">Home</a>')
+        end
+      end
+
+      context 'when the current path does not match the url' do
+        before do
+          @nav_item = @trejo.nav_item('Blog', '/blog', selected: /^\/blog/)
+        end
+
+        it 'returns a unselected nav item' do
+          expect(@nav_item).to eq('<a href="/blog">Blog</a>')
+        end
+      end
     end
   end
 end
