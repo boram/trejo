5dd4ea890d4369244477cff368448fd666ed8193
Boram Yoon <boram@me.com>
2015-01-18 14:16:49 -0800

    Add more helpers

--- lib/trejo.rb
+++ lib/trejo.rb
@@ -1,2 +1,22 @@
 require 'trejo/version'
 require 'trejo/railtie' if defined?(Rails)
+
+module Trejo
+  class << self
+    attr_accessor :configuration
+  end
+
+  def self.configure
+    self.configuration ||= Configuration.new
+    yield configuration
+  end
+
+  class Configuration
+    attr_accessor :site_title, :company_name
+
+    def initialize
+      @site_title = ''
+      @company_name = ''
+    end
+  end
+end
--- lib/trejo/view_helpers.rb
+++ lib/trejo/view_helpers.rb
@@ -18,5 +18,34 @@ module Trejo
         .reject(&:blank?).map(&:split)
         .flatten.uniq.join ' '
     end
+
+    def title page_title
+      return if page_title.blank?
+      content_for(:title) { "#{page_title} | #{Trejo.configuration.site_title}" }
+    end
+
+    def body_id
+      if content_for?(:page_id)
+        content_for(:page_id)
+      else
+        "#{controller.controller_name}-#{controller.action_name}-page".dasherize
+      end
+    end
+
+    def body_class
+      content_for?(:page_class) ? content_for(:page_class) : nil
+    end
+
+    def page_id value
+      content_for(:page_id) { value }
+    end
+
+    def page_class value
+      content_for(:page_class) { value }
+    end
+
+    def copyright_notice
+      "\u00A9 #{Date.current.year} #{Trejo.configuration.company_name}, All Rights Reserved"
+    end
   end
 end
